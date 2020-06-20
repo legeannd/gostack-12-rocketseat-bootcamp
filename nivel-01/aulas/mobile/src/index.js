17c1560c9a3a7968c6fd56d770fe4496a9baaaee
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-20 18:26:15 -0300

    Criação dos arquivos mobile + alteração no diretorio

---
+++ nivel-01/aulas/mobile/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold'
+  }
+});
