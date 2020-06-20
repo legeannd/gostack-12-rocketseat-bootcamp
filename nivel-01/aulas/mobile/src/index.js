23d3f723bfc01cfd7096b0601060793a6b1e0353
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-20 19:22:26 -0300

    Criação de um novo projeto via API pelo mobile

--- nivel-01/aulas/mobile/src/index.js
+++ nivel-01/aulas/mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -13,6 +13,17 @@ export default function App() {
     })
   }, []);
 
+  async function handleAddProject( ) {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Gean Lucas'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
@@ -25,6 +36,13 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -38,5 +56,17 @@ const styles = StyleSheet.create({
   project: {
     color: '#fff',
     fontSize: 30,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 });
