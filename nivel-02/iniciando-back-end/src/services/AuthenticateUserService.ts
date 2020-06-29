ef9ef33d072bc60753388626e8b3dde669912c04
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-29 14:47:53 -0300

    Bloqueando acesso de usuários não autenticados através do JWT + id do usuário acessível

--- nivel-02/iniciando-back-end/src/services/AuthenticateUserService.ts
+++ nivel-02/iniciando-back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -10,6 +13,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,8 +34,16 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    })
+
     return {
       user,
+      token,
     };
   }
 }
