b040d1f7005a4f5a4ad68f702daf96d1b0b199cf
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-29 02:31:50 -0300

    API cadastrando e fazendo requisições diretamente ao banco

--- nivel-02/iniciando-back-end/src/database/migrations/1593403303743-CreateAppointments.ts
+++ nivel-02/iniciando-back-end/src/database/migrations/1593403303743-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1593403303743 implements MigrationInterfa
               type: 'varchar',
               isPrimary: true,
               generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
             },
             {
               name: 'provider',
--- nivel-02/iniciando-back-end/src/repositories/AppointmentsRepository.ts
+++ nivel-02/iniciando-back-end/src/repositories/AppointmentsRepository.ts
@@ -1,39 +1,19 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-//Data Transfer Object
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment>{
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
+  public async findByDate(date: Date): Promise<Appointment | null> {
 
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+    const findAppointment = await this.findOne({
+      where: { date },
+    })
 
     return findAppointment || null;
   }
 
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({provider, date});
-
-    this.appointments.push(appointment);
-
-    return appointment;
-  }
 }
 
 export default AppointmentsRepository;
