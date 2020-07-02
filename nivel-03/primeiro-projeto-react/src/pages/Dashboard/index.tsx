5c5ee824085609ecd811e56e4f6fdc8f739a6147
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-02 14:45:43 -0300

    Sistema de navegação entre rotas implementado

--- nivel-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -70,7 +71,7 @@ const Dashboard: React.FC = () => {
 
     <Repositories>
       {repositories.map(repository => (
-        <a key={repository.full_name} href="teste">
+        <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
           <img
             src={repository.owner.avatar_url}
             alt={repository.owner.login}
@@ -80,7 +81,7 @@ const Dashboard: React.FC = () => {
             <p>{repository.description}</p>
           </div>
           <FiChevronRight  size={20}/>
-        </a>
+        </Link>
       ))}
     </Repositories>
   </>
