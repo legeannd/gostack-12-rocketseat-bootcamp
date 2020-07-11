1433545819fe97d0a8b81f1400d22f470da63b18
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-11 01:04:28 -0300

    Página de login  + Componentes de input e button modularizados

--- nivel-03/gobarber-web/src/App.tsx
+++ nivel-03/gobarber-web/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-};
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
