b5fbc7cacabcc409f85ebd8584f476e77da91eb9
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-12 16:25:10 -0300

    Criação das rotas da aplicação

--- nivel-03/appgobarber/src/App.tsx
+++ nivel-03/appgobarber/src/App.tsx
@@ -1,6 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App: React.FC = () => <View />;
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
