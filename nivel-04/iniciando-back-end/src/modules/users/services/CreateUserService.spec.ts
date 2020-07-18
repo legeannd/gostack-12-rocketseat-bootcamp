869ceff05b6dc11eda3dce3bed5195412e74f14f
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-18 16:41:04 -0300

    Desacoplando provedor de criação de hash de senhas + testes de autenticação finalizados

--- nivel-04/iniciando-back-end/src/modules/users/services/CreateUserService.spec.ts
+++ nivel-04/iniciando-back-end/src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,18 @@
 import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -18,7 +25,12 @@ describe('CreateUser', () => {
 
   it('should not be able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     await createUser.execute({
       name: 'John Doe',
