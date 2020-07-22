cacd15a200c2094bb7820551eaf398e802c115a1
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-22 01:29:14 -0300

    Testes de recuperação de senha finalizados

--- nivel-04/iniciando-back-end/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ nivel-04/iniciando-back-end/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -14,6 +14,8 @@ class FakeUserTokenRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
