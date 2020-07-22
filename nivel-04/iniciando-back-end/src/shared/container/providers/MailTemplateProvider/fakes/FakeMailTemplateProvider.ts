9a81c3a35910ab74f7ea8d9bf7661557347e26fd
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-22 15:34:36 -0300

    Criação do arquivo de template de recuperação de senha por email + integração com token

--- nivel-04/iniciando-back-end/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ nivel-04/iniciando-back-end/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailProvider from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailProvider {
-  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'MailContent';
   }
 }
 
--- nivel-04/iniciando-back-end/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ nivel-04/iniciando-back-end/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,14 +1,19 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailProvider from '../models/IMailTemplateProvider';
 
 class HandlebarsMailTemplateProvider implements IMailProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
