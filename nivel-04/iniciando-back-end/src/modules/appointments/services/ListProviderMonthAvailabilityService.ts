7322fbf9e7b9d476d7c7e36bad23e5d2d32b3a03
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-23 16:02:03 -0300

    Implementação da verificação de disponibilidade de agendamento no dia do mês

--- nivel-04/iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
-import User from '@modules/users/infra/typeorm/entities/User';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -34,9 +34,25 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
