c4d4d588bd708578312adb1db7724b059968c726
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-24 02:48:52 -0300

    Criação de rotas de listagem de dias e horários disponíveis para agendamento

--- nivel-04/iniciando-back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -22,7 +22,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
     return findAppointment;
   }
 
-  public async findAllInMonthFromProvider({
+  public async findAllInDayFromProvider({
     provider_id,
     day,
     month,
@@ -36,7 +36,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_char(${dateFieldName}, 'DD-MM-YYYY) = ${parsedDay}-${parsedMonth}-${year}`,
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
@@ -44,7 +44,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
     return appointments;
   }
 
-  public async findAllInDayFromProvider({
+  public async findAllInMonthFromProvider({
     provider_id,
     month,
     year,
@@ -56,7 +56,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
         provider_id,
         date: Raw(
           dateFieldName =>
-            `to_char(${dateFieldName}, 'MM-YYYY) = ${parsedMonth}-${year}`,
+            `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
         ),
       },
     });
--- nivel-04/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -28,7 +28,7 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('provider-id');
   });
 
-  it('should not be able to create two appointments in the same time', async () => {
+  it('should not be able to create two appointments in the same date', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date(2020, 4, 10, 12).getTime();
     });
@@ -60,20 +60,6 @@ describe('CreateAppointment', () => {
         date: new Date(2020, 4, 10, 11),
         user_id: 'user-id',
         provider_id: 'provider-id',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
-  });
-
-  it('should not be able to create an appointment with same user as provider', async () => {
-    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2020, 4, 10, 12).getTime();
-    });
-
-    await expect(
-      createAppointment.execute({
-        date: new Date(2020, 4, 10, 13),
-        user_id: 'user-id',
-        provider_id: 'user-id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
