20fd97ff1aa89cef5b5cbd342336e42bf823ba0f
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-25 15:56:58 -0300

    Validação no back-end dos dados enviados pelas rotas utilizando celebrate

--- nivel-04/iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -13,7 +14,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', appointmenstController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmenstController.create,
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
