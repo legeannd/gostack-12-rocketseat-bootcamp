7669dca8e4d6ca06c6156d7a9dae5c46dba2a97b
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-25 19:37:50 -0300

    Utilizando class-transformer para esconder e alterar dados enviados pelo servidor

--- nivel-04/iniciando-back-end/src/modules/users/infra/http/controllers/ProfileController.ts
+++ nivel-04/iniciando-back-end/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -12,9 +13,7 @@ export default class ProfileController {
 
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -32,9 +31,7 @@ export default class ProfileController {
         password,
       });
 
-      delete user.password;
-
-      return response.json(user);
+      return response.json(classToClass(user));
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
