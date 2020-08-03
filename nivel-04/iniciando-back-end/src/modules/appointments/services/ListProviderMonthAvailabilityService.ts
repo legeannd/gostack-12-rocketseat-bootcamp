6423640761b404276410a763f36a9698ad73ad3d
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-02 22:46:43 -0300

    Verificando as datas dos agendamentos para não marcá-los em datas que já passaram

--- nivel-04/iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ nivel-04/iniciando-back-end/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -38,17 +38,20 @@ class ListProviderMonthAvailabilityService {
 
     const eachDayArray = Array.from(
       { length: numberOfDaysInMonth },
-      (value, index) => index + 1,
+      (_, index) => index + 1,
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
