73efa9cf2c2e9bb90ea4e815f18fb7f40afb6f04
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-10 14:26:54 -0300

    Estruturação dos arquivos do front-end web do GoBarber no Nível 05

---
+++ nivel-05/gobarber-web/src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import { Container } from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Input: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container type="button" {...rest}>
+    {children}
+  </Container>
+);
+
+export default Input;
