d714cea9bf4391af8261ef1fb6f7f21e731fa03a
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-19 15:12:19 -0300

    Mostrando data do agendamento realizado na página de agendamento concluído

--- nivel-05/appgobarber/src/pages/AppointmentCreated/index.tsx
+++ nivel-05/appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import {
   Container,
   Title,
@@ -10,8 +12,15 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkPressed = useCallback(() => {
     reset({
@@ -20,16 +29,20 @@ const AppointmentCreated: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, est
-        atque reiciendis amet corporis iusto! Quas temporibus aliquam, magni
-        corporis quidem voluptatem, a esse totam, magnam rem iure id tempora!
-      </Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPressed}>
         <OkButtonText>Ok</OkButtonText>
