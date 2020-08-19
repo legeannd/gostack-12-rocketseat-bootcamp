b3d5ff947104e633a746bc2a79d604f0c39bcc25
Gean Lucas <gean.lfpsa@gmail.com>
2020-08-18 23:51:21 -0300

    Adicionando FlatList de cabeleireiros ao clicar em um provider

--- nivel-05/appgobarber/src/pages/CreateAppointment/index.tsx
+++ nivel-05/appgobarber/src/pages/CreateAppointment/index.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
 
 import {
   Container,
@@ -8,6 +9,8 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersList,
+  ProvidersListContainer,
 } from './styles';
 import { useAuth } from '../../hooks/auth';
 
@@ -15,11 +18,24 @@ interface RouteParams {
   providerId: string;
 }
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const CreateAppointment: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
   const { user } = useAuth();
   const route = useRoute();
   const { goBack } = useNavigation();
   const { providerId } = route.params as RouteParams;
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
   const navigateBack = useCallback(() => {
     goBack();
@@ -36,6 +52,18 @@ const CreateAppointment: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <HeaderTitle>{provider.name}</HeaderTitle>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- nivel-05/appgobarber/src/pages/CreateAppointment/styles.ts
+++ nivel-05/appgobarber/src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Provider } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -29,4 +31,12 @@ export const UserAvatar = styled.Image`
   height: 56px;
   border-radius: 28px;
   margin-left: auto;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
 `;
